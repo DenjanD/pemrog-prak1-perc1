ec169441177e450441a979b592e26e480794191a
DenjanD <alfien311@gmail.com>
2021-11-05 21:22:38 +0700

    push pertama kali

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P1_1_1204039
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Halo Dunia!");
+        }
+    }
+}
